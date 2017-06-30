6cd1fe6eeb8793948f08d2cc3b055856a466facd
Guillaume Secret <g.secret91@gmail.com>
2017-06-30 18:25:38 +0200

    trying to fix

--- Physique/Main.cpp
+++ Physique/Main.cpp
@@ -7,7 +7,22 @@
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-	// Complètement perdu sur le le main, testé beaucoup de choses avec toujours plus d'erreurs
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
